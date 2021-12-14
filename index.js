d04d683db1b07f80d0cda6350d16dbf630d20c3d
Aleks <ivaleks73@gmail.com>
2021-12-14 02:13:34 -0700

    commit tree fc245f158c21ffea8b7c0b853aa3315b7d21ac24

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function saturdayFun(){
+    return 'This Saturday, I want to roller-skate'
+}
+function saturdayFun(a = 'roller-skate'){
+    return (`This Saturday, I want to ${a}!`)
+}
+let mondayWork = function(b = 'go to the office'){
+    return (`This Monday, I will ${b}.`)
+}
+    
